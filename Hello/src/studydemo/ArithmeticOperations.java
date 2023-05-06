76ea7fb2affc7a6fb5747ad02341f04e279fe494
Shree <Shree@DESKTOP-EHTMRKQ>
2023-05-06 20:23:57 +0530

    first project

---
+++ Hello/src/studydemo/ArithmeticOperations.java
@@ -0,0 +1,13 @@
+package studydemo;
+
+public class ArithmeticOperations {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("addition:" +(a+b));
+		System.out.println("substraction:" +(b-a));
+		System.out.println("Multiplication:" +(a*b));
+		System.out.println("division:" +(b/a));
+	}
+
+}
