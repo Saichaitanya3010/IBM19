e5a266534b68baa6d4b98ce7e84f0aadab122155
Shree <Shree@DESKTOP-EHTMRKQ>
2023-05-07 20:48:11 +0530

    java features

---
+++ Java_Features/src/com/ibm/CurrencyDemo.java
@@ -0,0 +1,13 @@
+
+package com.ibm;
+import static com.ibm.cc.Currency.*;
+
+public class CurrencyDemo {
+	public static void main(String[] args) {
+		System.out.println(convert(USD,INR,10));
+		System.out.println(convert(UKP,USD,10));
+		System.out.println(convert(UKP,INR,10));
+		
+	}
+
+}
---
+++ Java_Features/src/com/ibm/Hello.java
@@ -0,0 +1,16 @@
+package com.ibm;
+@FunctionalInterface
+public interface Hello {
+	
+	
+		String sayHello();
+		
+		default String sayBye() {
+			return "Good Bye";
+		}
+		static void greetings() {
+			System.out.println("Hola Amigo!");
+		}
+
+	}
+
